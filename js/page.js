294382a7359599c9c6b70739e81b85c4e2eef411
Reece Emero <reece.emero98@gmail.com>
2019-04-01 15:42:47 -0400

    Added carousel functionality

--- js/page.js
+++ js/page.js
@@ -1,1 +1,32 @@
-console.log("loaded")
+/* Global variables */
+var slideIdx = 0;
+
+/* Toggle navigation bar */
+function navToggle() {
+    let nav = document.getElementById("main-nav");
+    if (!nav.classList.contains('responsive')) {
+        nav.classList.add('responsive')
+    } else {
+        nav.classList.remove('responsive')
+    }
+}
+
+/* Change slide to display slide n */
+function showSlides(n) {
+    let slides = document.getElementsByClassName("slide")
+    let indicators = document.getElementsByClassName("indicator")
+    n >= 0 ? slideIdx = n % slides.length : slideIdx = slides.length-1
+
+    for (let i = 0; i < slides.length; i++) {
+        if (i === slideIdx) {
+            slides[i].style.display = "block"
+            indicators[i].classList.add('active')
+        } else {
+            slides[i].style.display = "none"
+            indicators[i].classList.remove('active')
+        }
+    }
+}
+
+/* Initially show the first slide */
+showSlides(slideIdx);
